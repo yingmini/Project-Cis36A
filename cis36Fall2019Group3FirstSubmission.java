eaaea3c2d4b39e2f9563cfec0d48818f83271710
PhuongHuynh9987 <54336313+PhuongHuynh9987@users.noreply.github.com>
2019-09-27 09:55:27 -0700

    Add files via upload

---
+++ cis36Fall2019Group3FirstSubmission.java
@@ -0,0 +1,66 @@
+/*
+ * Program name: cis36Fall2019Group3FirstSubmission.java
+ * Discussion:   Part B Submission
+ * Written by: 	 Phuong Huynh, Ying, Fiker, Dominique
+ * Date:		 2019/09/24	
+ */
+
+public class cis36Fall2019Group3FirstSubmission {
+    public static void main(String[] arg){
+	System.out.println("a) Group Members: " +
+                "\tYing Hsuen Lin" + "\n\t\t\tPhuong Huynh" +
+                "\n\t\t\tFiker Workene" + 
+                "\n\t\t\tDominique Debergue" +
+                "\nb) Reading material: " +
+                "\thttps://rosettacode.org/wiki/Conway%27s_Game_of_Life" +
+                "\n\t\t\thttps://codereview.stackexchange.com/questions/" +
+                "\n\t\t\thttp://web.stanford.edu/class/archive/cs" +
+                "\n\t\t\thttps://medium.com/tebs-lab/" +
+                "optimizing-conways-game-of-life-12f1b7f2f54c" +
+                "/cs106b/cs106b.1178/assn/life.html" +
+                "126056/simple-conways-game-of-" +
+                "life-implementation-in-java" +
+                "\nc) JavaFX" +
+                "\nd) Gameboard: Ying" + "\n   Patterns: Fiker" + 
+                "\n   Graphics: Nick" + "\n   Rules: Phuong" +
+                "\n   We are going to have a research about " +
+                "how the game works." +
+                "\n   Then Ying will create " +
+                "the gameboard to start then share " +
+                "\n   it on Github so we can pull " +
+                "and work on our own part.");
+    }
+}
+
+/**
+ * PROGRAM OUTPUT
+ */
+
+/*
+a) Group Members: 	Ying Hsuen Lin
+			Phuong Huynh
+			Fiker Workene
+			Dominique Debergue
+b) Reading material: 	https://rosettacode.org/wiki/Conway%27s_Game_of_Life
+			https://codereview.stackexchange.com/questions/
+			http://web.stanford.edu/class/archive/cs/cs106b/
+                        cs106b.1178/assn/life.html126056/
+                        simple-conways-game-of-life-implementation-in-java
+                        https://medium.com/tebs-lab/
+                        optimizing-conways-game-of-life-12f1b7f2f54c
+c) JavaFX
+d) Gameboard: Ying
+   Patterns: Fiker
+   Graphics: Nick
+   Rules: Phuong
+   We are going to have a research about how the game works.
+   Then Ying will create the gameboard to start then share 
+   it on Github so we can pull and work on our own part.
+*/
+
+/**
+ * Logic_Code_Output
+ * 
+ * n/a
+ * 
+ */
